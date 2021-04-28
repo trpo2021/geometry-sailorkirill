a7c9cf76592483417a7db46b9fa3c254dc2527ee
Kirill Ryabov <sailor.kirill@gmail.com>
2021-04-29 00:31:21 +0700

    Add new figure - triangle

--- src/libgeometry/functions.h
+++ src/libgeometry/functions.h
@@ -21,17 +21,32 @@ typedef struct {
 } Circle;
 
 typedef struct {
+    int x[4];
+    int y[4];
+    int space[4];
+    int comma[3];
+    int open_bracket[2];
+    int close_bracket[2];
+} Triangle;
+
+typedef struct {
     double perimeter;
     double area;
 } Calculations;
+
+Calculations triangle_compute(Triangle points);
 
 Calculations circle_compute(double radius);
 
 int radius_check(char* figure, int indx1, int indx2);
 
+int triangle_point_reader(char *triangle_wkt, int boarder1, int boarder2);
+
 int point_check(char* figure, int indx1, int indx2);
 
 int control_count(int norm, int get_count);
+
+Triangle triangle_wkt_check(char* triangle_figure, int triangle_length);
 
 Circle circle_wkt_check(char* circle_figure, int circle_length);
 
